8599f4be1dbe9c51bc15ea2364bbd8a6027a568c
Julian Korgol <kontakt@juliankorgol.com>
2022-09-10 22:31:44 +0200

    App init and first configuration

--- DjangoShodanAPP/ShodanAPP/models.py
+++ DjangoShodanAPP/ShodanAPP/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Configuration(models.Model):
+    ShodanAPIKey = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.ShodanAPIKey
